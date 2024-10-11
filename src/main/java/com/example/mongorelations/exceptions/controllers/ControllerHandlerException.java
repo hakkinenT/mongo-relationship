1c633766e4e1c53ed6caa9b65a404757431eb4e5
Tawanna <thol_16@hotmail.com>
2024-10-11 20:21:10 -0300

    custom handler exception

---
+++ src/main/java/com/example/mongorelations/exceptions/controllers/ControllerHandlerException.java
@@ -0,0 +1,31 @@
+package com.example.mongorelations.exceptions.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.mongorelations.exceptions.customs.ResourceNotFoundException;
+import com.example.mongorelations.exceptions.models.StandardError;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerHandlerException {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Resource Not Found");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+}
